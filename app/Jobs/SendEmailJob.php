85b40370ebefaf80eaf7bddb3cb69e68159b87dd
Le Van Quan <quanbka.cntt@gmail.com>
2020-05-25 07:55:06 +0700

    email fix

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Mail;
 class SendEmailJob extends Job
 {
     /**
-     * Create a new job instance.
-     *
-     * @return void
-     */
+    * Create a new job instance.
+    *
+    * @return void
+    */
 
-     public $email;
-     public $data;
+    public $email;
+    public $data;
 
     public function __construct($email, $data)
     {
@@ -23,15 +23,15 @@ class SendEmailJob extends Job
     }
 
     /**
-     * Execute the job.
-     *
-     * @return void
-     */
+    * Execute the job.
+    *
+    * @return void
+    */
     public function handle()
     {
         \Log::info("Doing job");
         \Log::info($this->email);
-        $emails = explode("," $this->email);
+        $emails = explode(",", $this->email);
         foreach ($emails as $key => $value) {
             $emails[$key] = trim($value);
         }
